9e6ec8ba74efd65e2c78655a524bc1e8406135bf
Holly Draz <94925959+HollyDraz@users.noreply.github.com>
2025-01-21 13:01:02 -0600

    created server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+//server file for which ur app wil  live on 
+
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const {pool} = require('pg');
+
+//call the channel for your server
+const app = express();
+const port = 5000;
+
+//create the bridge to your db
